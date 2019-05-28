bfb6ff4c6ddb59f5f5ec54d3784a2270ce462a35
WillLai <252926752@qq.com>
2019-05-28 14:08:04 +0800

    Completed Lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,13 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
 		int x = 0;
+		int sum = 0;
 		while (x < 10) {
-			System.out.print(x + " ");
+			for (int i = 0; i < x+1; i = i + 1) {
+				sum = sum + i;
+			}
+			System.out.print(sum + " ");
+			sum = 0;
 			x = x + 1;
 		}
 	}
