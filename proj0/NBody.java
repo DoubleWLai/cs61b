08184779fcbf795a0d443acf623dcd8368b644ba
WillLai <252926752@qq.com>
2019-07-12 09:56:39 +0800

    add some changes to proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class NBody {
 
  //  Read radius from file 
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -12,7 +12,7 @@ public class Planet {
     public double yyVel;       // Its current velocity in the y direction
     public double mass;        // Its mass
     public String imgFileName; // The name of the file that corresponds to the image that depicts the planet
-    public static final double G = 6.67e-11; //gravitational constant 
+    private static final double G = 6.67e-11; //gravitational constant 
 
 // Initialize a Planet object.
     public Planet(double xP, double yP, double xV,
