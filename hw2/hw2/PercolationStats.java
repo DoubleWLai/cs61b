eee1d52616fb69e9cc56a88d869e1943cc970849
WillLai <252926752@qq.com>
2019-07-19 15:06:33 +0800

    update hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -2,14 +2,14 @@ package hw2;
 import edu.princeton.cs.introcs.StdRandom;
 
 public class PercolationStats {
-    private int[] numOfSite;
+    private double[] numOfSite;
 
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new java.lang.IllegalArgumentException("Illegal Argument");
         }
-        numOfSite = new int[T];
+        numOfSite = new double[T];
         for (int i = 0; i < T; i++) {
             Percolation perc = pf.make(N);
             while (!perc.percolates()) {
@@ -24,7 +24,9 @@ public class PercolationStats {
                 row = ((randomNum - col) / N) + 1;
                 perc.open(row, col);
             }
-            numOfSite[i] = perc.numberOfOpenSites();
+            double num = perc.numberOfOpenSites();
+            double p = num / (N * N);
+            numOfSite[i] = p;
         }
     }
 
@@ -33,7 +35,7 @@ public class PercolationStats {
     // sample mean of percolation threshold
     public double mean() {
         double i = 0;
-        for (int a : numOfSite) {
+        for (double a : numOfSite) {
             i = i + a;
         }
         return (i/numOfSite.length);
@@ -43,7 +45,7 @@ public class PercolationStats {
     public double stddev() {
         double u = mean();
         double i = 0;
-        for ( int a : numOfSite) {
+        for ( double a : numOfSite) {
             i = i + ((a - u) * (a - u));
         }
         return i / (numOfSite.length -1);
